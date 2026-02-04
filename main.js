8b39a85d7728d3db23d8b25fcdd89003cf2ff9bc
achaba-analytics254 <djangozent@gmail.com>
2026-02-04 03:50:10 +0300

    tried webforms

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+
 const openModal = document.getElementById('openModal')
 const closeForm = document.getElementById('close-form')
 const contactForm = document.getElementById('contact-form')
-const h1 = document.querySelector('h1')
 
 openModal.addEventListener('click', () => {
     contactForm.classList.add('show');
